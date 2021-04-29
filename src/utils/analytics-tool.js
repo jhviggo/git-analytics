a16e11df3aed7f41f69d649486de8c01acefedce
Viggo Petersen <vig56@hotmail.com>
2021-04-29 23:49:42 +0200

    Implement knowledge map
    issue #29
    
    - implement knowledge map function
    - add api route for it

--- src/utils/analytics-tool.js
+++ src/utils/analytics-tool.js
@@ -9,7 +9,7 @@ async function countFileChanges(log, exclude=[], sortBy='changeCount') {
       const fileName = file.isRename ? file.newName : file.name;
 
       // exclude files
-      if (exclude.includes(fileName)) continue;
+      if (exclude && exclude.includes(fileName)) continue;
       
       // file exists in hotspot
       if (Object.keys(hotspot).includes(fileName)) {
@@ -34,6 +34,37 @@ async function countFileChanges(log, exclude=[], sortBy='changeCount') {
     .sort((a, b) => b[sortBy] - a[sortBy]);
 }
 
+async function knowledgeMap(log) {
+  // { file: { counts: { devName: 123 }, totalCount: 123} }
+  const map = {};
+
+  for (const commit of log.commits) {
+    for (const file of commit.fileChanges) {
+      // add line if it does not exist
+      if (!Object.keys(map).includes(file.name)) {
+        map[file.name] = {
+          fileName: file.name,
+          counts: {},
+        }
+      }
+
+      // add developer to counts if they do not exist
+      const oldCount = Object.keys(map[file.name].counts).includes(commit.developer.name)
+        ? map[file.name].counts[commit.developer.name]
+        : 0;
+
+      map[file.name].counts[commit.developer.name] = Number(oldCount) + Number(file.lineCount);
+    }
+  }
+
+  for (const key of Object.keys(map)) {
+    map[key].totalCount = Object.values(map[key].counts).reduce((a, b) => a + b, 0);
+  }
+
+  return Object.values(map).sort((a, b) => b.totalCount - a.totalCount);
+}
+
 module.exports = {
   countFileChanges,
+  knowledgeMap,
 };
