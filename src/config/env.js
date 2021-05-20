17b557af1f19f82135727e4862c576a3770afd4e
Viggo Petersen <vig56@hotmail.com>
2021-05-20 20:18:13 +0200

    Fix max buffer bug
    issue #40
    
    - add max buffer env variable
    - set default to 200kb

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,8 @@
 const config = {
   'PORT': process.env.PORT || 1337,
   'LOGGING_LEVEL': process.env.LOGGING_LEVEL || 'short',
-  'CMD_PATH': process.env.CMD_PATH || '.'
+  'CMD_PATH': process.env.CMD_PATH || '.',
+  'STDIO_MAXBUFFER_KB': process.env.STDIO_MAXBUFFER_KB || 200,
 };
 
 module.exports = config;
